86717c20d9a7a0a05642eb0fef5ed27d31be2730
MatthijsKroesen <matthijs.kroesen@gmail.com>
2024-09-30 17:56:58 +0200

    Update DMBAproject.py

--- DMBAproject.py
+++ DMBAproject.py
@@ -11,13 +11,13 @@ def GenerateKnapsackData(n, w_max, alpha):
     v = np.zeros(n)
     for i in range(n):
         w[i] = np.random.randint(low=0, high=w_max)
-        v[i] = w[i] + np.random.randint(low=0, high=w_max//5) #we let c[i] be correlated with w[i] with added randomness
+        v[i] = w[i] + np.random.randint(low=1, high=w_max//5) #we let c[i] be correlated with w[i] with added randomness. Make sure we do not have 0's. 
     w_sum = np.sum(w)
     W = int(round(alpha * w_sum,0))
     return w,v,W
     
-n=5
-w_max = 5
+n=100
+w_max = 100
 alpha = 0.3
 w,v,W = GenerateKnapsackData(n, w_max, alpha)
 
@@ -50,9 +50,9 @@ def BinaryProgrammingKnapsack(n,w,v,W):
     
     for i in index:
         opt = x[i].X
-        print("x_{}: {}".format(i,opt))
+        #print("x_{}: {}".format(i,opt))
     opt_val = knapsack.ObjVal
-    print("Optimal value: {}".format(opt_val))
+    print("Optimal Value Gurobi: {}".format(opt_val))
     return opt_val
 
 opt_val_bin = BinaryProgrammingKnapsack(n, w, v, W)
@@ -61,23 +61,27 @@ opt_val_bin = BinaryProgrammingKnapsack(n, w, v, W)
 def DynamicProgrammingKnapsack(n,w,v,W):
     M = {}
     for j in range(W+1):
-        M[(-1,j)] = 0
-    for i in range(0,n):
-        for j in range(1,W+1):
+        M[(0,j)] = 0
+    for i in range(1,n+1):
+        for j in range(W+1):
             if w[i-1] > j:
                 M[(i,j)] = M[(i-1, j)]
             else:
-                M[(i, j)] = max(M[(i - 1, j)], v[i] + M[(i-1, j - w[i])])
-    opt = M[(n-1,W)]
-    print(opt)
+                M[(i, j)] = max(M[(i - 1, j)], v[i-1] + M[(i-1, j - w[i-1])])
+    opt = M[(n,W)]
+    print('Optimal Value Dynamic Programming: {}'.format(opt))
     return opt
 
 opt_val_dyn = DynamicProgrammingKnapsack(n,w,v,W)
         
-        
-
 #%% 3. Greedy Heuristic for KP
+def GreedyHeuristicKnapsack(n,w,v,W):
+    ratio = v/w
+    ratio_sorted = np.sort(ratio)
+    print(ratio_sorted)
+    
 
+GreedyHeuristicKnapsack(n,w,v,W)
 
 #%% 4. Using a NN to approximately solve the Dynamic Programming (Q-learning)
 
